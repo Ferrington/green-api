e2dfe864966ae1edf5bd10a8bfe06ea00fab9fdd
Christopher Bandurski <cbandurski1@gmail.com>
2023-10-17 16:27:00 -0500

    minor changes to product implementation

--- src/main/java/com/techelevator/green/controller/ProjectController.java
+++ src/main/java/com/techelevator/green/controller/ProjectController.java
@@ -12,38 +12,37 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/project")
+@CrossOrigin
 public class ProjectController {
 
     @Autowired
     ProjectService projectService;
 
     @GetMapping
-    @PreAuthorize("permitAll")
     public List<Project> listProjects() {
         return projectService.getAllProjects();
     }
 
     @GetMapping(path="/{id}")
-    @PreAuthorize("permitAll")
     public Project getProject(@PathVariable long id) {
         return projectService.getById(id);
     }
 
     @PostMapping
-    @PreAuthorize("hasRole('STUDENT')")
+    @PreAuthorize("hasAnyRole('STUDENT', 'ADMIN)")
     @ResponseStatus(value = HttpStatus.CREATED)
     public Project createProject(@RequestBody Project project) {
         return projectService.createProject(project);
     }
 
     @PutMapping(path="/{id}")
-    @PreAuthorize("hasRole('STUDENT')")
+    @PreAuthorize("hasAnyRole('STUDENT', 'ADMIN)")
     public Project updateProject(@PathVariable Long id, @RequestBody Project project, Principal principal) {
         return projectService.updateProject(id, project, principal);
     }
 
     @DeleteMapping(path="/{id}")
-    @PreAuthorize("hasRole('STUDENT')")
+    @PreAuthorize("hasAnyRole('STUDENT', 'ADMIN)")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteProject(@PathVariable Long id, Principal principal) {
         projectService.deleteProject(id, principal);
